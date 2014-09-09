440563a9cd92777cb3367b822e8be8567057c7af
Jeff Foster <jeff.foster@acm.org>
2014-09-09 11:48:38 +0100

    hello world

---
+++ OutlookMeetingScraper/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Exchange.WebServices;
+
+namespace OutlookMeetingScraper
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
