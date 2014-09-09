018a62283f074ed96907b764f621ba984f11a216
Jeff Foster <jeff.foster@acm.org>
2014-09-09 17:12:53 +0100

    Scrape all the data and perform basic rollup

--- OutlookMeetingScraper/EntryPoint.cs
+++ OutlookMeetingScraper/EntryPoint.cs
@@ -17,7 +17,7 @@ namespace OutlookMeetingScraper
 // ReSharper disable once PossibleNullReferenceException
             var userName = System.Console.ReadLine().Trim();
 
-            System.Console.WriteLine("Enter your password.  I won't steal it honest");
+            System.Console.WriteLine("Enter your password.  I won't steal it. Honest");
             var password = Console.ReadPassword();
 
             var service = new ExchangeService(ExchangeVersion.Exchange2007_SP1)
@@ -27,17 +27,62 @@ namespace OutlookMeetingScraper
                 TraceFlags = TraceFlags.None
             };
 
-            service.AutodiscoverUrl(userName, EwsExample.RedirectionUrlValidationCallback);    
-      
-            var meetings = new ExchangeRetriever(service).MeetingStatistics(DateTime.Now - TimeSpan.FromDays(365 * 3), DateTime.Now);
+            service.AutodiscoverUrl(userName, EwsExample.RedirectionUrlValidationCallback);
+
+            var startDate = DateTime.Now - TimeSpan.FromDays(365 * 6);
+            while (startDate.DayOfWeek != DayOfWeek.Monday)
+            {
+                startDate -= TimeSpan.FromDays(1);
+            }
+
+            var meetings = new ExchangeRetriever(service).MeetingStatistics(startDate, DateTime.Now);
 
             using (var output = File.CreateText("c:/temp/accepted_meetings.csv"))
             {
                 output.WriteLine("Meeting name,Meeting Date,Duration (hours)");
                 foreach (var meeting in meetings)
                 {
-                    output.WriteLine("{0},{1},{2}", meeting.Name, meeting.StartTime, meeting.Duration.TotalHours);
+                    output.WriteLine("{0},{1},{2}", meeting.Name.Replace(',',' '), meeting.StartTime, meeting.Duration.TotalHours);
                 }
+            }
+
+            using (var output = File.CreateText("c:/temp/meetings_per_week.csv"))
+            {
+                output.WriteLine("Week Number,Total Meeting Count,Duration (hours)");
+                var week = 0;
+
+                foreach (var meetingsGroupedByWeek in meetings.GroupBy(x => ((int) (Math.Floor((x.StartTime - startDate).TotalDays) / 7.0))))
+                {
+                    var total = meetingsGroupedByWeek.Aggregate(
+                        AggregatedMeeting.Zero, 
+                        (current, meetingInAWeek) => new AggregatedMeeting(current.TotalMeetings + 1, current.Duration + meetingInAWeek.Duration));
+
+                    output.WriteLine("{0},{1},{2}", week++,total.TotalMeetings, total.Duration.TotalHours);
+                }
+            }
+        }
+
+        public class AggregatedMeeting
+        {
+            private readonly int m_TotalMeetings;
+            private readonly TimeSpan m_Duration;
+
+            public static readonly AggregatedMeeting Zero = new AggregatedMeeting(0,TimeSpan.Zero);
+
+            public AggregatedMeeting(int totalMeetings, TimeSpan duration)
+            {
+                this.m_TotalMeetings = totalMeetings;
+                this.m_Duration = duration;
+            }
+
+            public int TotalMeetings
+            {
+                get { return m_TotalMeetings; }
+            }
+
+            public TimeSpan Duration
+            {
+                get { return m_Duration; }
             }
         }
     }
@@ -55,6 +100,11 @@ namespace OutlookMeetingScraper
         {
             var allMeetings = new List<Meeting>();
 
+            while (startDate.DayOfWeek != DayOfWeek.Monday)
+            {
+                startDate -= TimeSpan.FromDays(1);
+            }
+
             while (startDate < endDate)
             {
                 System.Console.WriteLine("Retrieving for {0}", startDate);
@@ -68,15 +118,20 @@ namespace OutlookMeetingScraper
                         ItemSchema.Subject,
                         AppointmentSchema.Start,
                         AppointmentSchema.Duration,
-                        AppointmentSchema.MyResponseType
+                        AppointmentSchema.MyResponseType,
+                        AppointmentSchema.IsAllDayEvent
                     )
                 };
 
                 // Retrieve a collection of appointments by using the calendar view.
                 var meetings = calendar.FindAppointments(cView);
 
+                
+
                 allMeetings.AddRange(
-                    meetings.Where(x => x.MyResponseType == MeetingResponseType.Accept).Select(meeting => new Meeting(meeting.Start, meeting.Duration, meeting.Subject))
+                    meetings.Where(x => x.MyResponseType == MeetingResponseType.Accept).Select(meeting => 
+                        new Meeting(meeting.Start, meeting.Duration > TimeSpan.FromHours(8) ? TimeSpan.FromHours(8) : meeting.Duration, meeting.Subject)
+                    )
                 );
 
                 startDate += TimeSpan.FromDays(28);
